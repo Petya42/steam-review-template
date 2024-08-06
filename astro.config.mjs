e593492570f05d0c1531f330321b6b1c344a4d83
Petya42 <peter.lajko001@gmail.com>
2024-08-06 11:44:05 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://vojtastruhar.github.io/",
-  base: "/steam-review-template",
+  site: "https://petya42.github.io/",
+  base: "/steam-hun-review-template",
   integrations: [tailwind()],
 });
